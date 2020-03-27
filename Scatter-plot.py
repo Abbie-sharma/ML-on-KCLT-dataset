fd8cc3a3875edf2252070ae86abae2c773a9a176
Abbie-sharma <49124688+Abbie-sharma@users.noreply.github.com>
2020-03-27 23:54:09 +0530

    Create Scatter-plot.py
    Plot two scatter plots between the following pairs of attributes: (i) actual_mean_temp & actual_min_temp and (ii) actual_mean_temp & actual_max_temp

---
+++ Scatter-plot.py
@@ -0,0 +1,5 @@
+df.plot.scatter("actual_mean_temp", "actual_min_temp")
+plt.title("Scatter plot b/w actual_mean_temp & actual_min_temp")
+
+df.plot.scatter("actual_mean_temp", "actual_max_temp")
+plt.title("Scatter plot b/w actual_mean_temp & actual_max_temp")
